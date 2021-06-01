4320c681063799d162418c836cba7acbd240db4c
sungyoonahn <51948435+sungyoonahn@users.noreply.github.com>
2021-06-01 15:53:10 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,84 +1,61 @@
+import os
 import torch
-from torch.utils.data import Dataset, DataLoader
+import torch.utils.data
 from torch import nn
-from torchvision import transforms
-import torchvision.models as models
 
-from dataload import CustomImageDataset
-from train import train, eval
-from utils import plot
+from dataloader import ColorHintDataset, tensor2im
+from train import train, validate
+# from myUnet import Unet
+from mynet2 import Unet
 
-# For Colab(Gdrive mount)
-# from google.colab import drive
-#
-# drive.mount("/content/gdrive")
 
-if __name__ == "__main__":
-    # Load device
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    print(device)
+def main():
+    ## DATALOADER ##
+    # Change to your data root directory
+    root_path = "data/"
+    # Depend on runtime setting
+    use_cuda = True
 
-    # Paths
-    # Data Path
-    data_path = "config_data"
-    # Output Path
-    save_path = "outputs"
+    train_dataset = ColorHintDataset(root_path, 128)
+    train_dataset.set_mode("training")
 
-    # Hyper Parameters
-    epoch =100
-    batch_size = 2
-    learning_rate = 1e-4
+    test_dataset = ColorHintDataset(root_path, 128)
+    test_dataset.set_mode("validation")
 
-    # Data Transform
-    transforms_train = transforms.Compose([transforms.Resize((256, 256)),
-                                           transforms.ToTensor()])
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset)
 
-    transforms_test = transforms.Compose([transforms.Resize((256, 256)),
-                                          transforms.ToTensor()])
+    model = Unet()
+    # print(model)
+    PATH = "model-epoch-8-losses-0.00763.pth"
+    model.load_state_dict(torch.load(PATH))
 
-    # Load Data
-    train_data_set = CustomImageDataset(data_set_path=data_path+"/train", transforms=transforms_train)
-    train_loader = DataLoader(train_data_set, batch_size=batch_size, shuffle=True)
+    criterion = nn.L1Loss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=0.0)
 
-    test_data_set = CustomImageDataset(data_set_path=data_path+"/val", transforms=transforms_test)
-    test_loader = DataLoader(test_data_set, batch_size=batch_size, shuffle=False)
-
-    if not (train_data_set.num_classes == test_data_set.num_classes):
-        print("error: Numbers of class in training set and test set are not equal")
-        exit()
-
-    # Model (resnet18)
-    num_classes = train_data_set.num_classes
-    resnet = models.resnet18(pretrained=True)
-    resnet.fc = nn.Linear(512, num_classes)
-    # Load Model
-    model = resnet.to(device)
-
-    # optimizer, loss function
-    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
-    criterion = nn.CrossEntropyLoss()
-
-    # Training Model
-    train_loss , train_accuracy = [], []
-    val_loss , val_accuracy = [], []
-
+    # Move model and loss function to GPU
+    if use_cuda:
+        criterion = criterion.cuda()
+        model = model.cuda()
+    # Make folders and set parameters
+    os.makedirs('outputs/GroundTruth', exist_ok=True)
+    os.makedirs('outputs/Hint', exist_ok=True)
+    os.makedirs('outputs/Output', exist_ok=True)
+    os.makedirs('checkpoints', exist_ok=True)
+    save_images = True
     best_losses = 1e10
-    name = "AdamW"
+    epochs = 250
+    # Train model
+    for epoch in range(epochs):
+        # Train for one epoch, then validate
+        train(train_dataloader, model, criterion, optimizer, epoch)
+        with torch.no_grad():
+            losses = validate(test_dataloader, model, criterion, save_images, epoch)
+        # Save checkpoint and replace old best model if current model is better
+        if losses < best_losses:
+            best_losses = losses
+            torch.save(model.state_dict(), 'checkpoints/model-epoch-{}-losses-{:.5f}.pth'.format(epoch + 1, losses))
 
-    for e in range(epoch):
-        # Train, Val
-        train_epoch_loss, train_epoch_acc = train(model, train_loader, optimizer, criterion, e)
-        val_epoch_loss, val_epoch_acc = eval(model, test_loader, criterion)
 
-        train_loss.append(train_epoch_loss)
-        train_accuracy.append(train_epoch_acc)
-        val_loss.append(val_epoch_loss)
-        val_accuracy.append(val_epoch_acc)
-
-        # Plot Acc and Loss for Train and Val Results
-        plot(train_accuracy, val_accuracy, train_loss, val_loss, save_path, name)
-
-        if val_epoch_loss < best_losses:
-            best_losses = val_epoch_loss
-            torch.save(model.state_dict(), save_path+
-                       '/model-epoch-{}-losses-{:.5f}.pth'.format(e + 1, val_epoch_loss))
+if __name__ == '__main__':
+    main()
